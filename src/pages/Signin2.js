28647e9b5cd0b0f37b72bde1c19c41849508834d
Tuntunkr <tuntunkumar04051998@gmail.com>
2026-05-26 22:36:12 +0530

    fix: update signin page and styles

--- src/pages/Signin2.js
+++ src/pages/Signin2.js
@@ -95,8 +95,8 @@ export default function Signin2() {
             </Card.Body>
           </Card>
         </Col>
-        <Col className="d-none d-lg-block">
-          <img src={require("../assets/images/onwards.jpg")} className="auth-img" alt="" />
+        <Col className="d-none d-lg-block auth-img-col">
+          <img src={require("../assets/images/careveli-vitamin-c-serum.png")} className="auth-img" alt="Careveli 15% Vitamin C Face Serum" />
         </Col>
       </Row>
     </div>
